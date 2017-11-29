9a30127764c795ba79237a97cb684c77dcc68b72
Raghu Betina <raghu@firstdraft.com>
2017-11-29 02:58:45 +0000

    run rails g active_admin:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root :to => "prescriptions#index"
   # Routes for the Data_type resource:
   # CREATE
